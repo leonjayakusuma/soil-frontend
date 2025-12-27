4a3588dffab83bf6e42e8c874cc36dc9f0113239
leonjayakusuma <leon.jayakusuma77@gmail.com>
2025-12-27 14:32:04 +0700

    fixed npm vulnerabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 import dotenv from "dotenv";
